18c0ac388aeecd2f7261b7c324c780be1179eddc
Nelly Simkova <sk8high3r@ya.ru>
2023-12-19 22:12:31 +0300

    Add toggle method to TasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(completed: params[:completed])
+
+    render json: { message: "Success" }
+  end
   private
 
   def task_params
